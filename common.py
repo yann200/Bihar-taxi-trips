76fdf3590df6bbac644a9f9115faa30e9e6e0021
Evgeniya ISHKINA <e.ishkina@net.estia.fr>
2023-12-06 09:01:54 +0100

    Fixed models folder creation

--- common.py
+++ common.py
@@ -34,6 +34,10 @@ def preprocess_data(X):
 
 def persist_model(model, path):
     print(f"Persisting the model to {path}")
+    model_dir = os.path.dirname(path)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
     with open(path, "wb") as file:
         pickle.dump(model, file)
     print(f"Done")
